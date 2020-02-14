812f137a8e78f0d95a329525c801dbf55ee499e2
ahotchkin <ahotchk1@yahoo.com>
2020-02-14 13:41:12 -0500

    Remove user_serializer.rb since it isn't needed

--- backend/app/controllers/api/v1/questions_controller.rb
+++ backend/app/controllers/api/v1/questions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::QuestionsController < ApplicationController
 
   def show
-    question = Question.find(params[:id])
-    render json: question.to_json
+    @question = Question.find(params[:id])
+    render json: @question.to_json
   end
 
 end
--- backend/app/services/user_serializer.rb
+++
@@ -1,9 +0,0 @@
-# is this needed?
-
-class UserSerializer
-
-  def initialize(user_object)
-    @user = user_object
-  end
-
-end
